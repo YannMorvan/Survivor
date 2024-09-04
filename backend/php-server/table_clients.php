947277a160c12398a8024efd2c211b07c7ad9c86
Yann <yann.perennes@epitech.eu>
2024-09-04 13:26:52 +0200

    feat(retrieve-client-profile-data): add php script to send data to the front

---
+++ backend/php-server/table_clients.php
@@ -0,0 +1,54 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+session_start();
+
+require_once __DIR__ . "/functions.php";
+require_once __DIR__ . "/db_connection.php";
+
+$query = "SELECT * FROM customers";
+
+$stm = $pdo->prepare($query);
+$stm->execute();
+$customers = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+if (empty($customers)) {
+    echo json_encode([
+        "status" => false,
+        "message" => "No customers found"
+    ]);
+    exit();
+}
+
+$customers_array = array_filter(array_map(function ($customer) use ($pdo) {
+
+    $paymentQuery = "SELECT * FROM payments WHERE id_customer = :id_customer";
+    $paymentStm = $pdo->prepare($paymentQuery);
+    $paymentStm->execute(["id_customer" => $customer["id"]]);
+    $payments = $paymentStm->fetch(PDO::FETCH_ASSOC);
+
+    $imageQuery = "SELECT * FROM images WHERE id_customer = :id_customer";
+    $imageStm = $pdo->prepare($imageQuery);
+    $imageStm->execute(["id_customer" => $customer["id"]]);
+    $images = $imageStm->fetch(PDO::FETCH_ASSOC);
+
+    if (empty($payments) || empty($images))
+        return null;
+
+    return [
+        "id" => $customer["id"],
+        "name" => $customer["name"],
+        "surname" => $customer['surname'],
+        "image" => base64_encode($images["image"]),
+        "email" => $customer["email"],
+        "phone_number" => $customer["phone_number"],
+        "payement_method" => $payments["method"],
+    ];
+}, $customers));
+
+echo json_encode([
+    "status" => true,
+    "data" => array_values($customers_array)
+]);
