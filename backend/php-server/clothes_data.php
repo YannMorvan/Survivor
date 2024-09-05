fa7f5bd497901b3fc54ba836db74b2f0d59b3e9a
YannMorvan <yann.morvan@epitech.eu>
2024-09-06 01:16:50 +0200

    feat(clothes): Add clothes matching page with both of the part finished

--- backend/php-server/clothes_data.php
+++ backend/php-server/clothes_data.php
@@ -1,10 +1,7 @@
 <?php
-
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: image/png');
-
 session_start();
-
 require_once __DIR__ . '/db_connection.php';
 require_once __DIR__ . '/functions.php';
 
@@ -17,7 +14,6 @@ if (!isset($_POST['type'])) {
 }
 
 $query = "SELECT ci.image FROM clothes c LEFT JOIN clothes_images ci ON c.id = ci.id_cloth WHERE c.type = :type";
-
 $stm = $pdo->prepare($query);
 $stm->execute(["type" => $_POST['type']]);
 $clothes = $stm->fetchAll(PDO::FETCH_ASSOC);
@@ -31,8 +27,11 @@ if (empty($clothes)) {
 }
 
 foreach ($clothes as $key => $cloth) {
-    $clothes[$key] = base64_encode($cloth['image']);
+    $clothes[$key]['image'] = base64_encode($cloth['image']);
 }
+
+// Log memory usage
+error_log("Memory usage: " . memory_get_usage());
 
 echo json_encode([
     "status" => true,
