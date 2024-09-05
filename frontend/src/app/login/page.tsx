63fba3f65435973f973f5afd6377adfbc4a3e523
Hugues Lejeune <hugues.lejeune@epitech.eu>
2024-09-05 16:42:02 +0200

    feat(devops): merge backend and frontend docker-compose.yml into one at root of repository

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+export default function Login() {
+    return (
+        <div>
+            <h2>Connexion</h2>
+            <form>
+            </form>
+            {/* <button id="Connexion" aria-label="Connexion" type="button" onClick={console.log("OK")}></button> */}
+        </div>
+    );
+}
