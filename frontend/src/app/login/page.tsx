a11b9b743b5974e8ad15823feded0f1c6c217df7
Hugues Lejeune <hugues.lejeune@epitech.eu>
2024-09-05 17:25:38 +0200

    feat(login-page): create page

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+
+export default function Login() {
+    return (
+        <div>
+            <form></form>
+            <button id="Connexion" aria-label="Connexion">Connexion</button>
+        </div>
+    );
+}
