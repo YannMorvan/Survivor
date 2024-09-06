e21a445f655fa52799d64e0cbdf464752160a079
Quentin <quentin.bollore@epitech.eu>
2024-09-07 01:24:41 +0200

    feat(charts): add data to chart

--- frontend/src/app/components/charts/meetings.jsx
+++ frontend/src/app/components/charts/meetings.jsx
@@ -3,9 +3,14 @@ import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
-const PieChartComponent = () => {
-  useEffect(() => {
+const parseDate = (dateStr) => {
+  const [day, month, year] = dateStr.split('/').map(Number);
+  return new Date(year, month - 1, day);
+};
 
+const PieChartComponent = ({ data }) => {
+
+  useEffect(() => {
     am4core.useTheme(am4themes_animated);
 
     let chart = am4core.create("chartdiv4", am4charts.PieChart);
@@ -44,7 +49,9 @@ const PieChartComponent = () => {
       am4core.color("#798BFF"),
       am4core.color("#B8ACFF"),
       am4core.color("#FFA9CE"),
-      am4core.color("#F8D974")
+      am4core.color("#F8D974"),
+      am4core.color("#FF6F61"),
+      am4core.color("#6B8E23"),
     ];
 
     chart.legend = new am4charts.Legend();
@@ -58,21 +65,54 @@ const PieChartComponent = () => {
 
     chart.legend.labels.template.fontSize = 12;
     chart.legend.labels.template.fill = am4core.color("#91A3B9");
-    
-    chart.data = [
-      { Sources: "Dating app", meetings: 301 },
-      { Sources: "Social media", meetings: 165 },
-      { Sources: "Outside", meetings: 139 },
-      { Sources: "Friends", meetings: 128 }
-    ];
   
+    const filteredData = data.filter((entry) => {
+      const date = parseDate(entry.date);
+      return date.getFullYear() === 2024 && date.getMonth() === 6;
+    });
+
+    const countOccurrences = (arr) => {
+      return arr.reduce((acc, item) => {
+        acc[item] = (acc[item] || 0) + 1;
+        return acc;
+      }, {});
+    };
+
+    const occurrences = countOccurrences(filteredData.map((entry) => entry.source));
+
+    const sortedOccurrences = Object.entries(occurrences)
+      .sort((a, b) => b[1] - a[1]);
+    console.log("Sorted Occurrences:", sortedOccurrences);
+
+    const topThree = sortedOccurrences.slice(0, 3);
+    const topThreeKeys = new Set(topThree.map(([key]) => key));
+
+    console.log(topThree)
+
+    const othersCount = sortedOccurrences
+      .filter(([key]) => !topThreeKeys.has(key))
+      .reduce((acc, [, count]) => acc + count, 0);
+
+    const chartData = [
+      ...topThree.map(([key, value]) => ({
+        Sources: key,
+        meetings: value,
+      })),
+      {
+        Sources: "Autres",
+        meetings: othersCount,
+      },
+    ];
+
+  
+    chart.data = chartData;
 
     return () => {
       if (chart) {
         chart.dispose();
       }
     };
-  }, []);
+  }, [data]);
 
   return <div id="chartdiv4" style={{ width: "100%", height: "300px" }}></div>;
 };
