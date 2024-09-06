299986b66a85895c04850d42844ff998ce3a5c8e
Quentin <quentin.bollore@epitech.eu>
2024-09-06 03:10:00 +0200

    feat(charts): add new colors

--- frontend/src/app/components/charts/progress.jsx
+++ frontend/src/app/components/charts/progress.jsx
@@ -3,7 +3,7 @@ import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
-const IncomeChart = () => {
+const IncomeChart = ({ data }) => {
   const chartRef = useRef(null);
 
   useEffect(() => {
@@ -15,8 +15,22 @@ const IncomeChart = () => {
       chart.logo.disabled = true;
     }
 
+    if (!data || data < 0 || data > 100) {
+      console.error("Invalid data for percentage chart. Data must be between 0 and 100.");
+      return;
+    }
+
+    // Déterminer la couleur en fonction des seuils
+    const getColor = (value) => {
+      if (value <= 60) return am4core.color("red");
+      if (value <= 75) return am4core.color("#F8DB7A");
+      if (value <= 90) return am4core.color("lightgreen");
+      return am4core.color("darkgreen");
+    };
+
+    // Assigner la couleur basée sur la valeur des données
     chart.data = [
-      { year: 2005, income: 23.5, color: chart.colors.next() }
+      { year: "2024", income: data, color: getColor(data) }
     ];
 
     let categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
@@ -24,16 +38,19 @@ const IncomeChart = () => {
     categoryAxis.renderer.inversed = true;
     categoryAxis.renderer.grid.template.disabled = false;
     categoryAxis.renderer.labels.template.disabled = true;
-    categoryAxis.renderer.line.strokeOpacity = 0;
+    categoryAxis.renderer.line.strokeOpacity = 0.5;
 
     let valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
-    valueAxis.renderer.labels.template.disabled = true;
+    valueAxis.min = 0;
+    valueAxis.max = 100;
+    valueAxis.renderer.labels.template.disabled = false;
+    valueAxis.renderer.grid.template.strokeOpacity = 0.5;
     valueAxis.renderer.line.strokeOpacity = 0;
 
     let series = chart.series.push(new am4charts.ColumnSeries3D());
     series.dataFields.valueX = "income";
     series.dataFields.categoryY = "year";
-    series.name = "Income";
+    series.name = "Percentage";
     series.columns.template.propertyFields.fill = "color";
     series.columns.template.tooltipText = "{valueX}";
     series.columns.template.column3D.stroke = am4core.color("#fff");
@@ -46,7 +63,7 @@ const IncomeChart = () => {
         chartRef.current.dispose();
       }
     };
-  }, []);
+  }, [data]);
 
   return (
     <div style={{ width: "100%", height: "120px" }}>
