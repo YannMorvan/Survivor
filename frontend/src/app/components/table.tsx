d9036b6db02523ca8c4ce4ec653dd3bc7962e2ce
Quentin <quentin.bollore@epitech.eu>
2024-09-06 03:11:19 +0200

    feat(table): add real data from api

--- frontend/src/app/components/table.tsx
+++ frontend/src/app/components/table.tsx
@@ -1,22 +1,20 @@
 "use client";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+import { Heart } from "lucide-react";
 
-const ClientTable = () => {
+const ClientTable = ({ data }) => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const [selectedFilter, setSelectedFilter] = useState("Last 30 days");
   const [searchTerm, setSearchTerm] = useState("");
-  const [checkedItems, setCheckedItems] = useState<number[]>([]);
+  const [checkedItems, setCheckedItems] = useState<Set<number>>(new Set());
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
   const router = useRouter();
 
-  const clients = [
-    { id: 12, name: "John Doe", sign: "Aries", address: "123 Main St", age: 28, gender: "Male" },
-    { id: 34, name: "Jane Smith", sign: "Taurus", address: "456 Elm St", age: 34, gender: "Female" },
-    { id: 56, name: "Bob Johnson", sign: "Gemini", address: "789 Oak St", age: 42, gender: "Male" },
-    { id: 78, name: "Alice Williams", sign: "Cancer", address: "101 Maple St", age: 25, gender: "Female" },
-    { id: 90, name: "Charlie Brown", sign: "Leo", address: "202 Pine St", age: 38, gender: "Male" },
-    { id: 21, name: "Diana Prince", sign: "Virgo", address: "303 Cedar St", age: 30, gender: "Female" },
-  ];
+  const clients = data?.data || [];
+  const totalPages = Math.ceil(clients.length / itemsPerPage);
 
   const filters = ["Last day", "Last 7 days", "Last 30 days", "Last month", "Last year"];
 
@@ -29,36 +27,51 @@ const ClientTable = () => {
 
   const handleSearchChange: (e: React.ChangeEvent<HTMLInputElement>) => void = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
-  };  
+    setCurrentPage(1);
+  };
 
-  const handleCheckboxChange: (index: number) => void = (index: number) => {
-    if (checkedItems.includes(index)) {
-      setCheckedItems((prev) => prev.filter((item) => item !== index));
-    } else {
-      if (checkedItems.length < 2) {
-        setCheckedItems((prev) => [...prev, index]);
+  const handleCheckboxChange: (clientId: number) => void = (clientId: number) => {
+    setCheckedItems((prev) => {
+      const newCheckedItems = new Set(prev);
+      if (newCheckedItems.has(clientId)) {
+        newCheckedItems.delete(clientId);
       } else {
-        alert("Vous ne pouvez sélectionner que 2 utilisateurs.");
+        if (newCheckedItems.size < 2) {
+          newCheckedItems.add(clientId);
+        } else {
+          alert("Vous ne pouvez sélectionner que 2 utilisateurs.");
+        }
       }
-    }
-  };  
+      return newCheckedItems;
+    });
+  };
 
   const handleCheckAll: () => void = () => {
-    if (checkedItems.length === clients.length) {
-      setCheckedItems([]);
+    if (checkedItems.size === clients.length) {
+      setCheckedItems(new Set());
     } else {
-      setCheckedItems(clients.map((_, index) => index).slice(0, 2));
+      const newCheckedItems = new Set(clients.slice(0, 2).map((client) => client.id));
+      setCheckedItems(newCheckedItems);
     }
   };
 
   const handleTryComptability: () => void = () => {
-    if (checkedItems.length === 2) {
-      const [firstClient, secondClient] = checkedItems.map((index) => clients[index].id);
-      router.push(`/astro/${firstClient}&${secondClient}`);
+    if (checkedItems.size === 2) {
+      const [firstClient, secondClient] = Array.from(checkedItems);
+      router.push(`/astro/${firstClient}/${secondClient}`);
     } else {
       alert("Veuillez sélectionner exactement 2 utilisateurs.");
     }
   };
+
+  const filteredClients = clients.filter((client) => {
+    const lowerCaseSearchTerm = searchTerm.toLowerCase();
+    return (
+      client.name.toLowerCase().includes(lowerCaseSearchTerm)
+    );
+  });
+
+  const paginatedClients = filteredClients.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
 
   return (
     <div className="relative overflow-x-auto sm:rounded-lg">
@@ -98,9 +111,7 @@ const ClientTable = () => {
             </div>
           )}
         </div>
-        <label htmlFor="table-search" className="sr-only">
-          Search
-        </label>
+        <label htmlFor="table-search" className="sr-only">Search</label>
         <div className="relative">
           <div className="absolute inset-y-0 left-0 flex items-center ps-3 pointer-events-none">
             <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
@@ -124,75 +135,86 @@ const ClientTable = () => {
               <div className="flex items-center">
                 <input
                   type="checkbox"
-                  checked={checkedItems.length === clients.length}
+                  checked={checkedItems.size === filteredClients.length}
                   onChange={handleCheckAll}
                   className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
               </div>
             </th>
             <th className="px-6 py-3">Name</th>
-            <th className="px-6 py-3">Address</th>
-            <th className="px-6 py-3">Sign</th>
-            <th className="px-6 py-3">Age</th>
-            <th className="px-6 py-3">Gender</th>
+            <th className="px-6 py-3">Surname</th>
+            <th className="px-6 py-3">Email</th>
+            <th className="px-6 py-3">Phone number</th>
           </tr>
         </thead>
         <tbody>
-          {clients
-            .filter((client) => client.name.toLowerCase().includes(searchTerm.toLowerCase()))
-            .map((client, index) => (
-              <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td className="w-4 p-4">
-                  <div className="flex items-center">
-                    <input
-                      type="checkbox"
-                      checked={checkedItems.includes(index)}
-                      onChange={() => handleCheckboxChange(index)}
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                  </div>
-                </td>
-                <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{client.name}</th>
-                <td className="px-6 py-4">{client.address}</td>
-                <td className="px-6 py-4">{client.sign}</td>
-                <td className="px-6 py-4">{client.age}</td>
-                <td className="px-6 py-4">{client.gender}</td>
-              </tr>
-            ))}
+          {paginatedClients.map((client) => (
+            <tr key={client.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+              <td className="w-4 p-4">
+                <div className="flex items-center">
+                  <input
+                    type="checkbox"
+                    checked={checkedItems.has(client.id)}
+                    onChange={() => handleCheckboxChange(client.id)}
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                </div>
+              </td>
+              <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                <img className="w-10 h-10 rounded-full" src={`data:image/png;base64,${client.image}`} alt="Client" />
+                <div className="ps-3">
+                  <div className="text-base font-semibold">{client.name}</div>
+                </div>
+              </th>
+              <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{client.surname}</th>
+              <td className="px-6 py-4">{client.email}</td>
+              <td className="px-6 py-4">{client.phone_number}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4" aria-label="Table navigation">
-            <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span className="font-semibold text-gray-900 dark:text-white">1-10</span> of <span className="font-semibold text-gray-900 dark:text-white">1000</span></span>
-            <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-                <li>
-                    <a href="#" className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
-                </li>
-                <li>
-                    <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-                </li>
-                <li>
-                    <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-                </li>
-                <li>
-                    <a href="#" aria-current="page" className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-                </li>
-                <li>
-                    <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
-                </li>
-                <li>
-                    <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
-                </li>
-                <li>
-            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
-                </li>
-            </ul>
-        </nav>
+        <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
+          Showing <span className="font-semibold text-gray-900 dark:text-white">{((currentPage - 1) * itemsPerPage) + 1}-{Math.min(currentPage * itemsPerPage, filteredClients.length)}</span> of <span className="font-semibold text-gray-900 dark:text-white">{filteredClients.length}</span>
+        </span>
+        <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
+          <li>
+            <button
+              onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Previous
+            </button>
+          </li>
+          {[...Array(totalPages)].map((_, index) => (
+            <li key={index}>
+              <button
+                onClick={() => setCurrentPage(index + 1)}
+                className={`flex items-center justify-center px-3 h-8 leading-tight ${currentPage === index + 1 ? "text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white" : "text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"}`}
+              >
+                {index + 1}
+              </button>
+            </li>
+          ))}
+          <li>
+            <button
+              onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Next
+            </button>
+          </li>
+        </ul>
+      </nav>
       <div className="py-4">
         <div className='flex lg:justify-end mt-2 mb-2'>
-            <div onClick={handleTryComptability} className='p-1.5 border border-slate-300 rounded bg-white cursor-pointer hover:bg-slate-100'>
-              <p className='text-slate-500 hover:text-slate-700 text-sm'>Try Comptability</p>
+          <div onClick={handleTryComptability} className='p-1.5 border border-slate-300 rounded bg-white cursor-pointer hover:bg-slate-100'>
+            <div className="flex">
+              <Heart size={16} className="mt-1" />
+              <span className="ms-2">Tester la compatibilité</span>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
