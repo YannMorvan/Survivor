a575ad04db82581e89a6b34c0827230a791602ff
Quentin <quentin.bollore@epitech.eu>
2024-09-09 01:13:59 +0200

    feat(comp/event): new event for components of the calendar

---
+++ frontend/src/app/components/event.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const getRandomColor: () => string = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+const getAfterComma: (str: string) => string = (str: string) => {
+    return str.split(',')[1];
+};
+
+const Event = ({ event, index, onEventClick }: any) => {
+    const { date, duration, name, location_name } = event;
+
+    const startDate = new Date(date);
+    const endDate = new Date(startDate.getTime() + duration * 60000);
+
+    const dayCount = Math.ceil((endDate.getTime() - startDate.getTime()) / (24 * 60 * 60 * 1000));
+
+    const verticalOffset = index * 30;
+
+    const handleClick = () => {
+        // When the event is clicked, pass event data to the parent
+        if (onEventClick) {
+            onEventClick(event);
+        }
+    };
+
+    return (
+        <div
+            className="event ml-2 cursor-pointer"
+            style={{
+                width: `${dayCount * 95}%`,
+                backgroundColor: getRandomColor(),
+                position: 'absolute',
+                top: `${verticalOffset}px`,
+                left: '0',
+                height: '30px',
+            }}
+            onClick={handleClick} // Attach the click handler
+        >
+            <span className="event-name p-5 font-semibold">
+                &#x2022; {name} ({getAfterComma(location_name)} )
+            </span>
+        </div>
+    );
+};
+
+export default Event;
