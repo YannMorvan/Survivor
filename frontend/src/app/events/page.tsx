94c9a2b1dfe78c25f11976020faeef6efa44eb43
Quentin <quentin.bollore@epitech.eu>
2024-09-09 01:11:07 +0200

    feat(events-page): add callback function + change map based on the event we click on

--- frontend/src/app/events/page.tsx
+++ frontend/src/app/events/page.tsx
@@ -1,11 +1,44 @@
 "use client";
 
-import React, { useState, useRef } from 'react';
-import { Plus, ChevronLeft, ChevronRight } from 'lucide-react';
+import React, { useState } from 'react';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 import Calendar from '../components/calendar';
 import Modal from '../components/modal';
 
 export default function Events() {
+
+    const [selectedMonth, setSelectedMonth] = useState('July 2024');
+    const [selectedEvent, setSelectedEvent] = useState(null);
+
+    const changeMonth = (direction: string) => {
+        const [currentMonthName, currentYear] = selectedMonth.split(' ');
+        const currentMonthIndex = new Date(Date.parse(currentMonthName + " 1, " + currentYear)).getMonth();
+        const currentYearNumber = parseInt(currentYear, 10);
+
+        const currentDate = new Date(currentYearNumber, currentMonthIndex);
+
+        currentDate.setMonth(currentDate.getMonth() + (direction === 'next' ? 1 : -1));
+
+        const newMonthName = currentDate.toLocaleString('default', { month: 'long' });
+        const newYear = currentDate.getFullYear();
+        
+        setSelectedMonth(`${newMonthName} ${newYear}`);
+    };
+
+    const handleEventClick = (event) => {
+        console.log('Event clicked:', event);
+        setSelectedEvent(event);
+    };
+
+    const getMapUrl = (event) => {
+        if (!event) return '';
+
+        const { location_x, location_y, location_name } = event;
+
+        return `https://maps.google.com/maps?width=100%25&height=600&hl=en&q=${location_x},${location_y}(${location_name})&t=&z=13&ie=UTF8&iwloc=B&output=embed`;
+    };
+
+
     return (
         <div className="ml-6 sm:mr-6 mr-6 mb-5">
             <div className='flex justify-between'>
@@ -18,9 +51,9 @@ export default function Events() {
                 <div>
                     <div className='md:flex justify-between'>
                         <div className='flex'>
-                            <p className='ml-10 mt-5 text-xl font-semibold'>July 2024</p>
-                            <ChevronLeft size={22} className='text-slate-400 cursor-pointer ml-5 mt-6'/>
-                            <ChevronRight size={22} className='text-slate-400 cursor-pointer ml-5 mt-6'/>
+                            <p className='ml-10 mt-5 text-xl font-semibold'>{selectedMonth}</p>
+                            <ChevronLeft size={22} className='text-slate-400 cursor-pointer ml-5 mt-6' onClick={() => changeMonth('prev')} />
+                            <ChevronRight size={22} className='text-slate-400 cursor-pointer ml-5 mt-6' onClick={() => changeMonth('next')} />
                         </div>
                         <div className='flex mt-5 md:ml-0 ml-10'>
                             <div>
@@ -47,18 +80,18 @@ export default function Events() {
                         </div>
                     </div>
                 </div>
-                <Calendar />
+                <Calendar month={selectedMonth} onEventClick={handleEventClick} />
             </div>
             <div className='mt-10 border bg-white p-5'>
-            <div className='w-full'>
-                <iframe
-                width="100%"
-                height="600"
-                scrolling="no"
-                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Boston,%20UK+(My%20Business%20Name)&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-                title="Google Maps"
-                />
-            </div>
+                <div className='w-full'>
+                    <iframe
+                        width="100%"
+                        height="600"
+                        scrolling="no"
+                        src={selectedEvent ? getMapUrl(selectedEvent) : ''}
+                        title="Google Maps"
+                    />
+                </div>
             </div>
         </div>
     );
