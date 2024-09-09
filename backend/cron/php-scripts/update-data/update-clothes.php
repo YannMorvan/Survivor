adb151b7d5e381148788f9b3ad975f6b88a5e566
Hugues Lejeune <hugues.lejeune@epitech.eu>
2024-09-09 14:47:22 +0200

    feat(cron): move update-data scripts to php-scripts dir

--- backend/cron/php-scripts/update-data/update-clothes.php
+++ backend/cron/php-scripts/update-data/update-clothes.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 session_start();
 
-require_once __DIR__ . "/functions.php";
+require_once __DIR__ . "/../functions.php";
 
 
 if (!isset($_ENV["API_KEY"])) {
--- backend/cron/php-scripts/update-data/update-encounters.php
+++ backend/cron/php-scripts/update-data/update-encounters.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 session_start();
 
-require_once __DIR__ . "/functions.php";
+require_once __DIR__ . "/../functions.php";
 
 
 if (!isset($_ENV["API_KEY"])) {
@@ -30,48 +30,48 @@ if (!isset($_POST["token"])) {
 $errors = [];
 
 
-$res = get_data_from_api($_ENV["API_KEY"], $_POST["token"], "https://soul-connection.fr/api/events");
+$res = get_data_from_api($_ENV["API_KEY"], $_POST["token"], "https://soul-connection.fr/api/encounters");
 
 if ($res["status"] == false) {
     echo json_encode([
         "status" => false,
-        "message" => "Error while getting all events from the API",
+        "message" => "Error while getting all encounters from the API",
         "errors" => $res["message"]
     ]);
     exit();
 }
 
-$events = $res["data"];
+$encounters = $res["data"];
 
 
-foreach ($events as $i => $event) {
-    if (!isset($event->id)) {
+foreach ($encounters as $i => $encounter) {
+    if (!isset($encounter->id)) {
         $errors[] = [
-            "context" => "Get an event from the API",
-            "error" => "No id found for the event"
+            "context" => "Get an encounter from the API",
+            "error" => "No id found for the encounter"
         ];
         continue;
     }
 
 
-    $res = get_data_from_api($_ENV["API_KEY"], $_POST["token"], "https://soul-connection.fr/api/events/" . $event->id);
+    $res = get_data_from_api($_ENV["API_KEY"], $_POST["token"], "https://soul-connection.fr/api/encounters/" . $encounter->id);
 
     if ($res["status"] == false) {
         $errors[] = [
-            "context" => "Get an event from the API",
+            "context" => "Get an encounter from the API",
             "error" => $res["message"]
         ];
         continue;
     }
 
-    $events[$i] = $res["data"];
+    $encounters[$i] = $res["data"];
 
 
-    $res = set_event_from_api_data($events[$i]);
+    $res = set_encounter_from_api_data($encounters[$i]);
 
     if ($res["status"] == false) {
         $errors[] = [
-            "context" => "Set an event in database from the API data",
+            "context" => "Set an encounter in database from the API data",
             "error" => $res["message"]
         ];
     }
@@ -80,6 +80,6 @@ foreach ($events as $i => $event) {
 
 echo json_encode([
     "status" => true,
-    "message" => "Events updated successfully",
+    "message" => "Encounters updated successfully",
     "errors" => $errors
 ]);
