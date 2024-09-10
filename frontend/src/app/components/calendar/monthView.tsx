e2ea65057c73c64c0cfc0d91c840779d54b1295d
Quentin <quentin.bollore@epitech.eu>
2024-09-10 10:03:58 +0200

    feat(calendar): mode liste done

--- frontend/src/app/components/calendar/monthView.tsx
+++ frontend/src/app/components/calendar/monthView.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Event from '../event';
 
-const MonthView = ({ events, month, onEventClick }: any) => {
-    
+const MonthView = ({ events, month, list, onEventClick }: any) => {
     const [monthName, year] = month.split(' ');
     const monthIndex = new Date(Date.parse(monthName + " 1, " + year)).getMonth();
     const yearNumber = parseInt(year, 10);
@@ -10,60 +9,98 @@ const MonthView = ({ events, month, onEventClick }: any) => {
     const getDaysInMonth = (monthIndex: number, year: number) => {
         return new Date(year, monthIndex + 1, 0).getDate();
     };
-    
+
     const daysInMonth = getDaysInMonth(monthIndex, yearNumber);
 
     const getDayEvents = (dayNumber: number) => {
         return events.filter((event: any) => {
             const eventDate = new Date(event.date);
-            return eventDate.getDate() === dayNumber;
+            return eventDate.getDate() === dayNumber && eventDate.getMonth() === monthIndex && eventDate.getFullYear() === yearNumber;
         });
     };
 
+    const allMonthEvents = Array.from({ length: daysInMonth }).reduce((acc: any[], _, dayIndex) => {
+        const dayNumber = dayIndex + 1;
+        const dayEvents = getDayEvents(dayNumber);
+        acc.push({ dayNumber, events: dayEvents });
+        return acc;
+    }, []);
+
     return (
-        <table className="w-full">
-            <thead>
-                <tr>
-                    {['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'].map(day => (
-                        <th key={day} className="p-2 border-r border-l h-10 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 xl:text-sm text-xs">
-                            <span className="xl:block lg:block md:block sm:block hidden">{day}</span>
-                            <span className="xl:hidden lg:hidden md:hidden sm:hidden block">{day.slice(0, 3)}</span>
-                        </th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {Array.from({ length: Math.ceil(daysInMonth / 7) }).map((_, rowIndex) => (
-                    <tr key={rowIndex} className="text-right mr-0 h-20">
-                        {Array.from({ length: 7 }).map((_, colIndex) => {
-                            const dayNumber = rowIndex * 7 + colIndex + 1;
-                            return dayNumber <= daysInMonth ? (
-                                <td
-                                    key={colIndex}
-                                    className="border pt-1 h-30 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 overflow-auto cell"
-                                >
-                                    <div className="flex flex-col h-36 xl:w-50 lg:w-50 md:w-50 sm:w-full w-10 overflow-hidden relative">
-                                        <div className="top h-5 w-full">
-                                            <span className="text-gray-500">{dayNumber}</span>
-                                        </div>
-                                        <div className="bottom flex-grow py-1 w-full cursor-pointer flex items-start justify-start relative">
-                                            {getDayEvents(dayNumber).map((event: any, index: number) => (
-                                                <Event key={event.id} event={event} index={index} onEventClick={onEventClick} />
-                                            ))}
-                                        </div>
-                                    </div>
-                                </td>
-                            ) : (
-                                <td
-                                    key={colIndex}
-                                    className="border p-1 h-30 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 overflow-auto"
-                                />
-                            );
-                        })}
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <div>
+            {list ? (
+                <div className="list-view">
+                    {allMonthEvents.length > 0 ? (
+                        allMonthEvents.map(({ dayNumber, events }: any, index: number) => (
+                            <div key={index} className="flex border-b pb-5 mt-5">
+                                <p className="text-2xl w-2/12">
+                                    {new Date(yearNumber, monthIndex, dayNumber).toLocaleDateString('default', { weekday: 'long', day: 'numeric', month: 'long' })}
+                                </p>
+                                <div className="w-10/12">
+                                    {events.length > 0 ? (
+                                        events.map((event: any, eventIndex: number) => (
+                                            <div key={eventIndex} className="mb-2 bg-sky-600 rounded p-1.5">
+                                                <p className="text-white cursor-pointer" onClick={() => onEventClick(event)}>
+                                                    {event.name}
+                                                </p>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        <p className="text-gray-400">Aucun événement</p>
+                                    )}
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <p className="text-center">Aucun événement pour ce mois.</p>
+                    )}
+                </div>
+            ) : (
+                <table className="w-full">
+                    <thead>
+                        <tr>
+                            {['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'].map(day => (
+                                <th key={day} className="p-2 border-r border-l h-10 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 xl:text-sm text-xs">
+                                    <span className="xl:block lg:block md:block sm:block hidden">{day}</span>
+                                    <span className="xl:hidden lg:hidden md:hidden sm:hidden block">{day.slice(0, 3)}</span>
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Array.from({ length: Math.ceil(daysInMonth / 7) }).map((_, rowIndex) => (
+                            <tr key={rowIndex} className="text-right mr-0 h-20">
+                                {Array.from({ length: 7 }).map((_, colIndex) => {
+                                    const dayNumber = rowIndex * 7 + colIndex + 1;
+                                    return dayNumber <= daysInMonth ? (
+                                        <td
+                                            key={colIndex}
+                                            className="border pt-1 h-30 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 overflow-auto cell"
+                                        >
+                                            <div className="flex flex-col h-36 xl:w-50 lg:w-50 md:w-50 sm:w-full w-10 overflow-hidden relative">
+                                                <div className="top h-5 w-full">
+                                                    <span className="text-gray-500">{dayNumber}</span>
+                                                </div>
+                                                <div className="bottom flex-grow py-1 w-full cursor-pointer flex items-start justify-start relative">
+                                                    {getDayEvents(dayNumber).map((event: any, index: number) => (
+                                                        <Event key={event.id} event={event} index={index} onEventClick={onEventClick} />
+                                                    ))}
+                                                </div>
+                                            </div>
+                                        </td>
+                                    ) : (
+                                        <td
+                                            key={colIndex}
+                                            className="border p-1 h-30 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 overflow-auto"
+                                        />
+                                    );
+                                })}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
     );
 };
 
--- frontend/src/app/components/calendar/weekView.tsx
+++ frontend/src/app/components/calendar/weekView.tsx
@@ -7,7 +7,7 @@ interface WeekViewProps {
     startDate: Date; // Date de début de la semaine
 }
 
-const WeekView: React.FC<WeekViewProps> = ({ events, onEventClick, startDate }) => {
+const WeekView = ({ events, onEventClick, list, startDate }: any) => {
     const daysInWeek = 7;
 
     const getDayEvents = (dayIndex: number) => {
@@ -16,7 +16,6 @@ const WeekView: React.FC<WeekViewProps> = ({ events, onEventClick, startDate }) 
 
         return events.filter((event: any) => {
             const eventDate = new Date(event.date);
-
             return (
                 eventDate.getDate() === currentDay.getDate() &&
                 eventDate.getMonth() === currentDay.getMonth() &&
@@ -25,45 +24,83 @@ const WeekView: React.FC<WeekViewProps> = ({ events, onEventClick, startDate }) 
         });
     };
 
-    return (
-        <table className="w-full h-full h-[500px]">
-            <thead>
-                <tr>
-                    {['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'].map(day => (
-                        <th key={day} className="p-2 border-r border-l h-10 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 xl:text-sm text-xs">
-                            <span className="xl:block lg:block md:block sm:block hidden">{day}</span>
-                            <span className="xl:hidden lg:hidden md:hidden sm:hidden block">{day.slice(0, 3)}</span>
-                        </th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                <tr className="text-right mr-0 h-20">
-                    {Array.from({ length: daysInWeek }).map((_, colIndex) => {
-                        const currentDay = new Date(startDate);
-                        currentDay.setDate(currentDay.getDate() + colIndex);
+    const allWeekEvents = Array.from({ length: daysInWeek }).map((_, dayIndex) => {
+        const currentDay = new Date(startDate);
+        currentDay.setDate(currentDay.getDate() + dayIndex);
+        const dayEvents = getDayEvents(dayIndex);
+        return { currentDay, dayEvents };
+    });
 
-                        return (
-                            <td
-                                key={colIndex}
-                                className="border pt-1 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 overflow-auto cell"
-                            >
-                                <div className="flex flex-col h-full xl:w-50 lg:w-50 md:w-50 sm:w-full w-10 overflow-hidden relative">
-                                    <div className="top h-5 w-full">
-                                        <span className="text-gray-500">{currentDay.getDate()}</span>
-                                    </div>
-                                    <div className="bottom flex-grow py-1 w-full cursor-pointer flex items-start justify-start relative">
-                                        {getDayEvents(colIndex).map((event: any, index: number) => (
-                                            <Event key={event.id} event={event} index={index} onEventClick={onEventClick} />
-                                        ))}
-                                    </div>
+    return (
+        <div>
+            {list ? (
+                <div className="list-view">
+                    {allWeekEvents.length > 0 ? (
+                        allWeekEvents.map(({ currentDay, dayEvents }, index) => (
+                            <div key={index} className="flex border-b pb-5 mt-5">
+                                <p className="text-2xl w-2/12">
+                                    {currentDay.toLocaleDateString('default', { weekday: 'long', day: 'numeric', month: 'long' })}
+                                </p>
+                                <div className="w-10/12">
+                                    {dayEvents.length > 0 ? (
+                                        dayEvents.map((event: any, eventIndex: number) => (
+                                            <div key={eventIndex} className="mb-2 bg-sky-600 rounded p-1.5">
+                                                <p className="text-white cursor-pointer" onClick={() => onEventClick(event)}>
+                                                    {event.name}
+                                                </p>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        <p className="text-gray-400">Aucun événement</p>
+                                    )}
                                 </div>
-                            </td>
-                        );
-                    })}
-                </tr>
-            </tbody>
-        </table>
+                            </div>
+                        ))
+                    ) : (
+                        <p className="text-center">Aucun événement cette semaine.</p>
+                    )}
+                </div>
+            ) : (
+                <table className="w-full h-full h-[500px]">
+                    <thead>
+                        <tr>
+                            {['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'].map(day => (
+                                <th key={day} className="p-2 border-r border-l h-10 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 xl:text-sm text-xs">
+                                    <span className="xl:block lg:block md:block sm:block hidden">{day}</span>
+                                    <span className="xl:hidden lg:hidden md:hidden sm:hidden block">{day.slice(0, 3)}</span>
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="text-right mr-0 h-20">
+                            {Array.from({ length: daysInWeek }).map((_, colIndex) => {
+                                const currentDay = new Date(startDate);
+                                currentDay.setDate(currentDay.getDate() + colIndex);
+
+                                return (
+                                    <td
+                                        key={colIndex}
+                                        className="border pt-1 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 overflow-auto cell"
+                                    >
+                                        <div className="flex flex-col h-full xl:w-50 lg:w-50 md:w-50 sm:w-full w-10 overflow-hidden relative">
+                                            <div className="top h-5 w-full">
+                                                <span className="text-gray-500">{currentDay.getDate()}</span>
+                                            </div>
+                                            <div className="bottom flex-grow py-1 w-full cursor-pointer flex items-start justify-start relative">
+                                                {getDayEvents(colIndex).map((event: any, index: number) => (
+                                                    <Event key={event.id} event={event} index={index} onEventClick={onEventClick} />
+                                                ))}
+                                            </div>
+                                        </div>
+                                    </td>
+                                );
+                            })}
+                        </tr>
+                    </tbody>
+                </table>
+            )}
+        </div>
     );
 };
 
