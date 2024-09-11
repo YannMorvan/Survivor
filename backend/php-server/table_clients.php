5884a32a504351a05ab2ec2179eb2d9f75b6dfc8
Hugues Lejeune <hugues.lejeune@epitech.eu>
2024-09-11 10:27:42 +0200

    fix(php): correct an issue where the older payment was gotten to retrieve payment method

--- backend/php-server/table_clients.php
+++ backend/php-server/table_clients.php
@@ -36,11 +36,11 @@ try {
 
     $customers_array = array_filter(array_map(function ($customer) use ($pdo) {
 
-        $paymentQuery = "SELECT * FROM payments WHERE id_customer = :id_customer ORDER BY date";
+        $paymentQuery = "SELECT * FROM payments WHERE id_customer = :id_customer ORDER BY date DESC";
 
         $paymentStm = $pdo->prepare($paymentQuery);
         $paymentStm->execute(["id_customer" => $customer["id"]]);
-        $payments = $paymentStm->fetchAll(PDO::FETCH_ASSOC);
+        $payments = $paymentStm->fetch(PDO::FETCH_ASSOC);
 
         return [
             "id" => $customer["id"],
@@ -48,7 +48,7 @@ try {
             "surname" => $customer['surname'],
             "email" => $customer["email"],
             "phone_number" => $customer["phone_number"],
-            "payement_method" => $_SESSION['is_coach'] ? (empty($payments) ? [] : $payments[0]["method"]) : [],
+            "payement_method" => $_SESSION['is_coach'] ? (empty($payments) ? [] : $payments["method"]) : [],
             "country" => $customer["country"],
             "country_code" => get_country_code($customer["country"]),
         ];
